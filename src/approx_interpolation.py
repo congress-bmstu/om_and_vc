cd0083a0d2d0e2c52eca9329183a8bc80001009b
Alexander Kortenko <akortenko03@gmail.com>
2024-04-03 01:26:26 +0300

    fixed mnk_interpolate

--- src/approx_interpolation.py
+++ src/approx_interpolation.py
@@ -1,30 +1,32 @@
 import numpy as np
+import sympy as sp
 import matplotlib.pyplot as plt
 
-def mnk_interpolate(X, Y, power=1):
-    F = np.matrix([[xi**pow for pow in range(power+1)] for xi in X])
+def mnk_interpolate(X, Y, power=1, x = sp.Symbol('x')):
+    F = sp.Matrix([[xi**pow for pow in range(power+1)] for xi in X])
     print(f'{F = }')
-    FTF = F.T @ F
-    print(f'{FTF = }')
-    FTY = F.T @ Y
-    print(f'{FTY = }')
-    beta = FTF.I @ F.T @ Y
-    beta = [i for i in np.array(beta[0])][0][::-1]
+    FTF = F.transpose() @ F
+    print(f'FT * F = {FTF}')
+    FTY = F.transpose() @ sp.Matrix(Y)
+    print(f'FT * Y = {FTY}')
+    beta = FTF.inv() @ FTY
+    # beta = [i for i in np.array(beta[0])][0][::-1]
     print(f'{beta = }')
     
-    p = np.poly1d(beta)
+    # p = np.poly1d(beta)
+    p = sum([ x**p * beta[p] for p in range(power+1) ])
     print(p,'\n')
     return p
 
 if __name__ == "__main__":
-    X = np.array([
+    X = [
         -1,0,1,2
-    ])
-    Y = np.array([
+    ]
+    Y = [
         1,-1,2,-1
-    ])
+    ]
 
-    p = mnk_interpolate(X, Y, 2)
+    p = sp.lambdify(sp.Symbol('x'), mnk_interpolate(X, Y, 2))
 
     plt.scatter(X,Y,color='red')
 

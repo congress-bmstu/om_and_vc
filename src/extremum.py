d97a0d57ad39411e0744eaefdf4030a9988d5f98
Alexander Kortenko <akortenko03@gmail.com>
2024-04-02 22:48:43 +0300

    work on method_lomannih

--- src/extremum.py
+++ src/extremum.py
@@ -1,21 +1,11 @@
 import numpy as np
 import sympy as sp
 
-from accurate_interpolation import *
-
-# TODO переделать структуру файла, чтобы был блок __name__ == "__main__",
-# убрать использование глобальных переменных, поработать над неймингом 
-# функций и переменных
-
-x = sp.Symbol('x')
-f = x**3 + 6 * x**2 + 9 * x + 1
-
-a = -3
-b = 0
+from .accurate_interpolation import *
 
 # ищется минимум
 
-def print_number(a, ROUNDING_COUNT = 5):
+def print_number(a, ROUNDING_COUNT = 5, x = sp.Symbol('x')):
     out = ''
     if(type(a) == int or type(a) == float):
         out += f'{round(a, ROUNDING_COUNT)}'
@@ -23,12 +13,14 @@ def print_number(a, ROUNDING_COUNT = 5):
         out += f'{a} = {round(a.evalf(), ROUNDING_COUNT)}'
     return out
 
-def bisect(f, a, b, iteration = 1, COUNT_ITERATIONS = 3):
+def bisect(f, a, b, iteration = 1,
+           COUNT_ITERATIONS = 3,
+           x = sp.Symbol('x'),
+           delta = sp.Rational(1, 10)):
     if(iteration > COUNT_ITERATIONS):
         print(f'Ну здесь остановимся, минимум будет в отрезке {a.evalf()}, {b.evalf()}')
         return
     func = sp.lambdify(x, f)
-    delta = sp.Rational(1, 10)
 
     u1 = sp.Rational( a+b - delta, 2)
     u2 = sp.Rational( a+b + delta, 2)
@@ -54,7 +46,7 @@ def bisect(f, a, b, iteration = 1, COUNT_ITERATIONS = 3):
         print(f'Так как на этом подотрезке функция убывает, выбираем отрезок [u1 = {print_number(u1)}, b = {print_number(b)}]')
         return bisect(f, u1, b, iteration+1, COUNT_ITERATIONS)
 
-def golden_cut(f, a, b, iteration = 1, COUNT_ITERATIONS = 3):
+def golden_cut(f, a, b, iteration = 1, COUNT_ITERATIONS = 3, x = sp.Symbol('x')):
     if(iteration > COUNT_ITERATIONS):
         print(f'Ну здесь остановимся, минимум будет в отрезке {a.evalf()}, {b.evalf()}')
         return
@@ -87,7 +79,7 @@ def golden_cut(f, a, b, iteration = 1, COUNT_ITERATIONS = 3):
 def is_vipuklaya_troika(a, fa, b, fb, c, fc):
     return fa >= fb and fc >= fb and fa + fc >= 2 * fb
 
-def parabola(f, a, b):
+def parabola(f, a, b, x = sp.Symbol('x')):
     func = sp.lambdify(x, f)
     h = 0.2
     us = [ (a+b)/ 2. ]
@@ -134,21 +126,117 @@ def parabola(f, a, b):
                 return 
             i += 1
 
-def method_lomannih(f, a, b):
-    pass
-    # TODO
 
-print('Метод деления отрезка пополам:')
-bisect(f, a, b)
+def find_piecewise_min(piecewise, a, b, x = sp.Symbol('x')):
+    min_x = None
+    min_piecewise = None
+    for (piece, interval) in piecewise.as_expr_set_pairs():
+        bounds = [interval.inf, interval.sup]
+        for b in bounds:
+            func_1 = piece.subs(x, b) 
+            if(min_piecewise is None or func_1 < min_piecewise):
+                min_x = b
+                min_piecewise = func_1
+    return min_x
 
-print('\n\nМетод золотого сечения:')
-golden_cut(f, a, b)
+def max_piecewise(piecewise_1, piecewise_2, point, x = sp.Symbol('x')):
+    expr_set = list(piecewise_1.as_expr_set_pairs())
+    expr_set_2 = list(piecewise_2.as_expr_set_pairs())
+    # предполагается, что piecewise_2 состоит из двух интервалов с границей point
+    interval_index = None
+    is_going_down = None
+    for (index, (piece, interval)) in enumerate(expr_set):
+        if interval.contains(point):
+            interval_index = index
+            is_going_down = (sp.diff(piece, x) < 0)
+            break
+    if(interval_index is None):
+        raise ValueError("Что-то пошло не так, не могу найти максимум ломанных")
+    # print(f"{interval_index = }, {is_going_down = }")
+    if(interval_index == 0 and not is_going_down):
+        deleted = expr_set.pop(0)
+        if(len(expr_set_2) == 1): # случай, когда sympy удалил левый подотрезок, потому
+            # что точка point = a.
+            x_1 = sp.solve(sp.Eq(deleted[0], expr_set_2[0][0]), x)[0]
+            expr_set += [
+                (expr_set_2[0][0], (deleted[1].inf <= x) & (x < x_1) ),
+                (deleted[0],       (x_1 <= x) & (x < deleted[1].sup) )]
+        else:
+            x_1 = sp.solve(sp.Eq(deleted[0], expr_set_2[1][0]), x)[0]
+            expr_set += [
+                (expr_set_2[0][0], (deleted[1].inf <= x) & (x < point) ),
+                (expr_set_2[1][0], (point <= x) & (x < x_1) ),
+                (deleted[0],       (x_1 <= x) & (x < deleted[1].sup) )]
+    elif(interval_index == len(expr_set)-1 and is_going_down):
+        deleted = expr_set.pop(interval_index)
+        x_1 = sp.solve(sp.Eq(deleted[0], expr_set_2[0][0]), x)[0]
+        expr_set += [
+            (deleted[0],       (deleted[1].inf <= x) & (x < x_1) ),
+            (expr_set_2[0][0], (x_1 <= x) & (x < point) ),
+            (expr_set_2[1][0], (point <= x) & (x < deleted[1].sup)) ]
+    else:
+        if(is_going_down):
+            deleted_2 = expr_set.pop(interval_index+1)
+            deleted_1 = expr_set.pop(interval_index)
+        else:
+            deleted_2 = expr_set.pop(interval_index)
+            deleted_1 = expr_set.pop(interval_index-1)
+        x_1 = sp.solve(sp.Eq(deleted_1[0], expr_set_2[0][0]), x)[0]
+        x_2 = sp.solve(sp.Eq(deleted_2[0], expr_set_2[1][0]), x)[0]
+        # print(expr_set)
 
-print('\n\nМетод парабол:')
-parabola(f, a, b)
+        expr_set += [
+                (deleted_1[0],    (deleted_1[1].inf <= x) & (x < x_1) ),
+                (expr_set_2[0][0], (x_1 <= x) & (x < deleted_1[1].sup) ),
+                (expr_set_2[1][0], (deleted_2[1].inf <= x) & (x < x_2) ),
+                (deleted_2[0],     (x_2 <= x) & (x < deleted_2[1].sup) )]
+    piecewise_args = [
+        (piece, interval.contains(x) if isinstance(interval, sp.Interval) else interval)
+        for (piece, interval) in expr_set]
+    return sp.Piecewise(*piecewise_args)
 
-print('\n\nМетод ломанных')
-method_lomannih(f, a, b)
+def method_lomannih(f, a, b, x0=None, L = None,
+                    x = sp.Symbol('x'),
+                    COUNT_ITERATIONS = 5):
+    if L is None:
+        f_diff_lambda = sp.lambdify(x, sp.diff(f, x))
+        L = max([ abs(f_diff_lambda(i)) for i in range(int(a), int(b)) ])
+    print(f'{L = }')
+    if x0 is None:
+        x0 = sp.Rational(a+b, 2)
+    u = sp.Symbol('u')
+    g = sp.Piecewise((f.subs(x, u) + L * (x-u), (a <= x) & (x < u) ),
+                     (f.subs(x, u) - L * (x-u), (x >= u) & (x <= b) ))
+    xi = [x0]
+    ps = [g.subs(u, x0)]
+    for i in range(COUNT_ITERATIONS):
+        print(f"{i+1}) x_{i} = {xi[i]}")
+        print(f"p_{i}")
+        sp.pprint(ps[-1])
+
+        xi.append( find_piecewise_min(ps[-1], a, b, x=x) )
+        ps.append( max_piecewise(ps[-1], g.subs(u, xi[-1]), xi[-1], x=x) )
+    return xi[-1]
+
+    
+
+if __name__ == "__main__":
+    x = sp.Symbol('x')
+    f = x**3 + 6 * x**2 + 9 * x + 1
+    a = -3
+    b = 0
+
+    print('Метод деления отрезка пополам:')
+    bisect(f, a, b)
+
+    print('\n\nМетод золотого сечения:')
+    golden_cut(f, a, b)
+
+    print('\n\nМетод парабол:')
+    parabola(f, a, b)
+
+    print('\n\nМетод ломанных')
+    method_lomannih(f, a, b, x0=-2)
 
 
 
